fa2892e440c569ca1a928f80bd7beb2351f52baf
mohan <mohan.chanti4@gmail.com>
2019-06-19 19:02:01 +0530

    update file

--- src/Hellokranthi.java
+++ src/Hellokranthi.java
@@ -2,7 +2,7 @@
 public class Hellokranthi {
 
 	public static void main(String[] args) {
-	System.out.println("Hello kranthi123777777777777");
+	System.out.println("Hello kranthi123777777777777gggggggggggggggg");
 
 	}
 
